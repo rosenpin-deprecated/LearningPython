ae27c021bfb6710a9001e49d6f247c78ffa05382
tomer.rosenfeld@protonmail.com <tomer.rosenfeld@protonmail.com>
2018-02-04 19:00:08 +0200

    Added basic HTTP server

--- sockets/learn_sockets.py
+++ sockets/learn_sockets.py
@@ -1,9 +1,32 @@
 from socket import *
+import os
+import atexit
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect(('smtp.gmail.com', 587))
-s.send('HELO smtp.gmail.com\n\n')
-print s.recv(10000)
-s.send('STARTTLS')
-print s.recv(10000)
-s.send('')
+
+s.bind(("0.0.0.0", 80))
+s.listen(5)
+
+atexit.register(lambda: s.close())
+try:
+    while True:
+        (connection, addr) = s.accept()
+        print "connected to ", addr
+        command = str(connection.recv(1000)).split(" ")
+        if command[0] != "GET":
+            connection.sendall("Only GET pls :)")
+            connection.close()
+        else:
+            path = os.getcwd() + command[1].split("\\")[0]
+            print "file to open %s" % path
+            if os.path.isfile(path):
+                f = open(path).read()
+                response = "HTTP/1.0 200 Document Follows\n" + "Content-length:" + str(len(f)) + "\n\n" + f
+
+            else:
+                response = "HTTP/1.0 404 Not Found\r\n"
+
+            connection.sendall(response)
+        connection.close()
+except Exception as e:
+    s.close()
